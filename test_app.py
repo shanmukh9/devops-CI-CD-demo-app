41e3830ac05c853f1f6419edfe1fb9f4eb947197
Shanmukh <yalavarthisainadh@gmail.com>
2022-03-06 08:38:37 -0500

    edited test

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from app import app
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello Everyone! Thank you for joining'
+    assert response.data == b'Hello Everyone! from Devops admin Shanmukh'
     assert response.status_code == 200
