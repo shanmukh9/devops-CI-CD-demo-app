cd13661b908d9cf3a35fbca8c58fdc598f080538
Shanmukh <yalavarthisainadh@gmail.com>
2022-03-06 08:33:31 -0500

    edited app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Everyone! Thank you for joining"
+    return "Hello Everyone! from Devops admin"
 
 # run the app.
 if __name__ == "__main__":
