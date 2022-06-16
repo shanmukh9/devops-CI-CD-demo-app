e2bf75e1cbd1a934ca817172fb50051481133399
YSS <yalavarthisainadh@gmail.com>
2022-06-16 13:42:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Everyone! from DevOps Engineer on Heroku !!!!"
+    return "Hello Everyone! from Hyderabad!!!!"
 
 # run the app.
 if __name__ == "__main__":
